a7aee2da2dc950795654248a756ad6bf1780317c
Raul Curelici <raul_curelici@yahoo.com>
2019-09-24 13:04:52 +0300

    add clean:dist task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require('gulp')
-//const del = require('del')
+const del = require('del')
 
 //plugins gulp
 const concat = require('gulp-concat')
-const terser = require('gulp-terser')
+const terser = require('gulp-terser')       //al posto di uglify, accetta ES6
 const less = require('gulp-less')
 const minifyCss = require('gulp-cssnano')
 const prefix = require('gulp-autoprefixer')
@@ -15,27 +15,32 @@ gulp.task('hello', (done) => {
     done()
 })
 
-
+gulp.task('clean:dist', () => {
+    return del(['dist'])
+})
 
 gulp.task('scripts', () => {
-    return gulp.src('/src/**/*.js')
+    return gulp.src('src/**/*.js')
     .pipe(concat('main.js'))
     .pipe(terser())
-    .pipe(gulp.dest('/dist/scripts'))
+    .pipe(gulp.dest('dist/scripts'))
 })
 
 gulp.task('styles', () => {
-    return gulp.src('/public/styles/**/*.less')
+    return gulp.src('public/styles/**/*.less')
     .pipe(less())
     .pipe(minifyCss())
     .pipe(prefix())
-    .pipe(gulp.dest('/dist/public/styles'))
+    .pipe(gulp.dest('dist/public/styles'))
 })
 
 gulp.task('tests', () => {
-    return gulp.src('/src/**/*.js')
-    .pipe(jshint({esversion: 6, asi : true}))       //opzioni: 1)abilita ES6, 2) rimuove errore per semicolon
+    return gulp.src('src/**/*.js')
+    .pipe(jshint())       //opzioni: 1)abilita ES6, 2) rimuovi errore per semicolon
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'))
 })
 
+gulp.task('default',() => {
+    //todo
+})
